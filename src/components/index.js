4634734f7e317fb49b0d6a86d15387a2b7dcc422
Yossi Spektor <easpex@gmail.com>
2020-03-21 14:23:16 +0200

    Not use export from syntax.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,9 @@
-export { default as Bio } from "./Bio"
-export { default as Layout } from "./Layout"
-export { default as SEO } from "./seo/SEO"
+// export { default as Bio } from "./Bio"
+// export { default as Layout } from "./Layout"
+// export { default as SEO } from "./seo/SEO"
+
+import Bio from "./Bio"
+import Layout from "./Layout"
+import SEO from "./seo/SEO"
+
+export { Bio, Layout, SEO }
