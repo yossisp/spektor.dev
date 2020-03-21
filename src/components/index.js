10499fb4be64008504a88488e3b67547a5a2892d
Yossi Spektor <easpex@gmail.com>
2020-03-21 19:22:50 +0200

    Use babel plugin-proposal-export-default-from.

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,3 @@
-// export { default as Bio } from "./Bio"
-// export { default as Layout } from "./Layout"
-// export { default as SEO } from "./seo/SEO"
-
-import Bio from "./Bio"
-import Layout from "./Layout"
-import SEO from "./seo/SEO"
-
-export { Bio, Layout, SEO }
+export { default as Bio } from "./Bio"
+export { default as Layout } from "./Layout"
+export { default as SEO } from "./seo/SEO"
